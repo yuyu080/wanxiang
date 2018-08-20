d567ad6afbc0d01d6a96d31caac97dabee8abbdf
Ezio <huangweizhe@bbdservice.com>
2018-08-20 18:25:18 +0800

    fix some typos

--- src/to_local/create_index.py
+++ src/to_local/create_index.py
@@ -5,37 +5,39 @@ import sys
 
 version = sys.argv[1]
 
+print("start to create indexes")
 
 flag = subprocess.call(
     '''
     /data1/wanxiangneo4jpre/neo4j-enterprise-3.4.0/bin/cypher-shell \
      -a bolt://10.28.62.46:30050 \
-     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Company(bbd_qyxx_id);";
+     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Company(bbd_qyxx_id)";
      /data1/wanxiangneo4jpre/neo4j-enterprise-3.4.0/bin/cypher-shell \
      -a bolt://10.28.62.46:30050 \
-     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Person(bbd_qyxx_id);";
+     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Person(bbd_qyxx_id)";
      /data1/wanxiangneo4jpre/neo4j-enterprise-3.4.0/bin/cypher-shell \
      -a bolt://10.28.62.46:30050 \
-     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Role(bbd_role_id);";
+     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Role(bbd_role_id)";
      /data1/wanxiangneo4jpre/neo4j-enterprise-3.4.0/bin/cypher-shell \
      -a bolt://10.28.62.46:30050 \
-     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Event(bbd_event_id);";
+     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Event(bbd_event_id)";
      /data1/wanxiangneo4jpre/neo4j-enterprise-3.4.0/bin/cypher-shell \
      -a bolt://10.28.62.46:30050 \
-     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Region(region_code);";
+     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Region(region_code)";
      /data1/wanxiangneo4jpre/neo4j-enterprise-3.4.0/bin/cypher-shell \
      -a bolt://10.28.62.46:30050 \
-     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Industry(industry_code);";
+     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Industry(industry_code)";
      /data1/wanxiangneo4jpre/neo4j-enterprise-3.4.0/bin/cypher-shell \
      -a bolt://10.28.62.46:30050 \
-     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Time(time);";
+     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Time(time)";
      /data1/wanxiangneo4jpre/neo4j-enterprise-3.4.0/bin/cypher-shell \
      -a bolt://10.28.62.46:30050 \
-     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Company(address);";
+     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Company(address)";
      /data1/wanxiangneo4jpre/neo4j-enterprise-3.4.0/bin/cypher-shell \
      -a bolt://10.28.62.46:30050 \
-     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Contact(bbd_contact_id);";
-    '''
+     -u neo4j -p fyW1KFSYNfxRtw1ivAJOrnV3AKkaQUfC "CREATE INDEX ON :Contact(bbd_contact_id)";
+    ''',
+    shell=True
 )
 
 subprocess.call(
